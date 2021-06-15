db5a46a41f5f3b29f3778400775749d098f866bd
Krzysztof Kozak <hithard5@gmail.com>
2021-06-15 16:15:46 +0200

    first commit

---
+++ promise.js
@@ -0,0 +1,17 @@
+const createUserConsentPromise = (duration) => {
+  function getUserConsent(resolve, reject) {
+    setTimeout(() => {
+      resolve('User consented');
+    }, duration);
+  }
+  return (userConsentPromise = new Promise(getUserConsent));
+};
+
+const onUserConsent = (promiseObject) => console.log(promiseObject);
+const onUserDecline = (promiseObject) => console.log(promiseObject);
+
+const shortPromise = createUserConsentPromise(3000);
+const longPromise = createUserConsentPromise(10000);
+
+shortPromise.then(onUserConsent.bind(this, shortPromise), onUserDecline);
+longPromise.then(onUserConsent.bind(this, longPromise), onUserDecline);
