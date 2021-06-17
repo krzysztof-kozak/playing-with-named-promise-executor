0324c7903d186f268ed409881190f6a8a70a7a33
Krzysztof Kozak <hithard5@gmail.com>
2021-06-17 21:30:16 +0200

    Promise.race and error handling

--- promise-all.js
+++ promise-all.js
@@ -15,13 +15,28 @@ async function getItem(key, delayInMs) {
 }
 
 async function getItemsFromSomeAPI() {
-  const dataItem1 = getItem('Some item', 4000);
-  const dataItem2 = getItem('Some other item', 2000);
+  try {
+    const dataItem1 = getItem('Some item', 4000);
+    const dataItem2 = getItem('Some other item', 2000);
+    const item = await Promise.race([dataItem1, dataItem2]);
 
-  // item that was rejected or resolved the fastest
-  const item = await Promise.race([dataItem1, dataItem2]);
+    throw 'Something went wrong!';
 
-  return [item];
+    //this will return the item that was resolved the quickest, unless there was an error
+    return [item];
+  } catch (error) {
+    console.log(error);
+    return 'We are going to be fine';
+    // throw 'This is broken!';
+
+    // if we return a value , we basically ignore the error and provide a replacement value
+    // the consumer of this promise will get the replacement value inside of then callback
+
+    // if we throw an error, it will break the consumer's promise chain
+    // the error value will get passed to the catch callback
+  }
 }
 
-getItemsFromSomeAPI().then((result) => log(result));
+getItemsFromSomeAPI()
+  .then((result) => console.log({ result }))
+  .catch((error) => console.log({ error }));
