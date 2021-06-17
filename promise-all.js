8f54ce3c7d4f5698e811498786e07909df68cb9b
Krzysztof Kozak <hithard5@gmail.com>
2021-06-17 21:46:39 +0200

    Trying out Promise.allSettled

--- promise-all.js
+++ promise-all.js
@@ -6,37 +6,36 @@ const db = {
   'Some other item': 097823,
 };
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = (ms, shouldReject) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-async function getItem(key, delayInMs) {
+async function getItem(key, delayInMs, shouldReject = false) {
+  if (shouldReject) {
+    return new Promise((_, reject) => setTimeout(() => reject('rejected'), delayInMs));
+  }
+
   const item = db[key];
-  await delay(delayInMs);
+  await delay(delayInMs, shouldReject);
+
   return item;
 }
 
 async function getItemsFromSomeAPI() {
   try {
-    const dataItem1 = getItem('Some item', 4000);
-    const dataItem2 = getItem('Some other item', 2000);
-    const item = await Promise.race([dataItem1, dataItem2]);
+    const dataItem1 = getItem('Some item', 1000);
+    const dataItem2 = getItem('Some other item', 1000);
+    const dataItem3 = getItem('Some item', 1000, true);
 
-    throw 'Something went wrong!';
+    const item = await Promise.allSettled([dataItem1, dataItem2, dataItem3]);
 
-    //this will return the item that was resolved the quickest, unless there was an error
     return [item];
   } catch (error) {
     console.log(error);
     return 'We are going to be fine';
-    // throw 'This is broken!';
-
-    // if we return a value , we basically ignore the error and provide a replacement value
-    // the consumer of this promise will get the replacement value inside of then callback
-
-    // if we throw an error, it will break the consumer's promise chain
-    // the error value will get passed to the catch callback
   }
 }
 
 getItemsFromSomeAPI()
-  .then((result) => console.log({ result }))
+  .then((result) => console.log(...result))
   .catch((error) => console.log({ error }));
